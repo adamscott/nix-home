2dafbf4f3089dd13f8a9b2d758b6f50a26cf5fb0
Adam Scott <ascott.ca@gmail.com>
2017-06-17 21:38:25 -0400

    Try to find a way.

--- lib/nixhome/default.nix
+++ lib/nixhome/default.nix
@@ -1,9 +1,8 @@
 { stdenv, pkgs }:
 let
-  mkHome = { user, files, environment }: let
+  mkHome = { user, files, environment ? {} }: let
     src = pkgs.writeText "${user}-nix-home.json" (builtins.toJSON {
        inherit files;
-       inherit environment;
     });
 
   writeFiles = ./../writeFiles.py;
@@ -11,7 +10,8 @@ let
   in stdenv.mkDerivation {
       name = "${user}-nix-home";
       inherit src;
-      
+      inherit environment;
+
       builder = pkgs.writeText "builder.sh" ''
         #!/bin/sh
 
@@ -20,7 +20,7 @@ let
         mkdir -p $out
         ${pkgs.python}/bin/python ${writeFiles} "$src" "$out"
       '';
-  } ++ environment;
+  };
 in
 {
   inherit mkHome;
